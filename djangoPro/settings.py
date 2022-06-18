1f0a6c26dfc8b3f60c49ef3c3ddb338778a39284
bekzod0525 <bekzodshermatov2005@gmail.com>
2022-06-18 19:56:33 +0500

    temlpatesni inmini ozgartirdim

--- djangoPro/settings.py
+++ djangoPro/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'djangoPro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templats'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
